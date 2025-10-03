19a38f34576be0c9467321ace6404f60c5e9a90d
Giftfx-ship <nwodugift5@gmail.com>
2025-10-03 22:33:37 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+module.exports = {
+  // WhatsApp phone number with country code, no "+" sign
+  phoneNumber: '233266309343',
+
+  // Optional: set true if you want to print QR (not needed for 6-digit flow)
+  printQR: false,
+};
