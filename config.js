e089697fd0e6c018ab16cb64e44dbc1b319fa342
Giftfx-ship <nwodugift5@gmail.com>
2025-10-04 09:31:04 +0100

    Update and rename config. to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  OWNER_JID: '2349164624021@s.whatsapp.net', // your WhatsApp number with country code
+  PHONE_FOR_PAIR: '2349164624021',           // number to request 6-digit pairing code
+  PRINT_QR: false,                            // no QR needed, uses 6-digit code
+  PREFIX: '.'
+};
